5af41e31fff0d28f565c728444a098a14314003f
Nephistar <51949062+Nephistar@users.noreply.github.com>
2026-07-18 13:08:01 +0200

    enable reading reference color values from file

---
+++ Reader.py
@@ -0,0 +1,36 @@
+import os
+import csv
+import oklch
+
+def _read_csv(filename: str):
+    with open(filename) as csvfile:
+        data = csv.reader(csvfile)
+        table = list(data)
+    return table
+
+def _get_column(table: list, col_idx: int):
+    column = [row[col_idx] for row in table]
+    return column
+
+def _create_lookup(ids: list, values: list):
+    dictionary = dict(zip(ids, values))
+    return dictionary
+
+def _parse_hexcode_to_rgb(hexcode: str):
+    hexcode = hexcode.lstrip(' #')
+    if hexcode.startswith('0x'):
+        hexcode = hexcode[2:]
+    rgb = tuple(int(hexcode[i:i+2], 16) for i in (0, 2, 4))
+    return rgb
+
+def create_oklch_dict_from_hexcodes(filename: str):
+    if not os.path.isabs(filename):
+        filename = './tables/' + filename
+    table = _read_csv(filename)
+    ids = _get_column(table, 0)
+    hexcodes = _get_column(table, 1)
+    rgbs = [_parse_hexcode_to_rgb(hexcode) for hexcode in hexcodes]
+    oklchs = [oklch.srgb_to_oklch(rgb) for rgb in rgbs]
+    lookup = _create_lookup(ids, oklchs)
+    return lookup
+
