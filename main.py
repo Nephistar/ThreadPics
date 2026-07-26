d4cf462ab7afd1058757024ec81bef4aec263097
Nephistar <51949062+Nephistar@users.noreply.github.com>
2026-07-26 11:18:46 +0200

    include reference in main example

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from ColorCard import ColorCard
 from OklchPlotter import OklchPlotter
+import Reader
 
 
 def example():
     card = ColorCard(img_format='png')
     thread_pic = card.create_thread_pic('666')
-    plotter = OklchPlotter(thread_pic)
+    lookup_file = 'lord_libidan_hexcodes.csv'
+    lookup = Reader.create_oklch_dict_from_hexcodes(lookup_file)
+    reference = lookup[thread_pic.thread_id]
+    plotter = OklchPlotter(thread_pic, reference)
     plotter.plot_combo_lch()
     plotter.save('')
     plotter.show()
