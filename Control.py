274d00506c6863414efcf7a50a10781ee4c18aa0
Nephistar <51949062+Nephistar@users.noreply.github.com>
2026-07-30 14:49:47 +0200

    wrap main in docopt & move example to Control

---
+++ Control.py
@@ -0,0 +1,30 @@
+from ColorCard import ColorCard
+from OklchPlotter import OklchPlotter
+import Reader
+import Writer
+
+class Control:
+    def __init__(self, args: dict):
+        self.args = args
+        self.run()
+
+    def run(self):
+        if self.args.get('example'):
+            self.example()
+
+    def example(self):
+        card = ColorCard(img_format='png')
+        thread_pic = card.create_thread_pic('666')
+        lookup_file = 'lord_libidan_hexcodes.csv'
+        lookup = Reader.create_oklch_dict_from_hexcodes(lookup_file)
+        reference = lookup[thread_pic.thread_id]
+        csv_lines = [Writer.get_stats_csv_header()]
+        plotter = OklchPlotter(thread_pic, reference)
+        csv_lines.append(Writer.get_stats_csv_line(plotter))
+        Writer.save_stats_csv(csv_lines, 'stats_666.csv')
+        plotter.plot_combo_lch()
+        plotter.save('')
+        plotter.show()
+
+
+
--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-from ColorCard import ColorCard
-from OklchPlotter import OklchPlotter
-import Reader
-import Writer
+"""
+ThreadPics
+https://github.com/Nephistar/ThreadPics
+License: GPL-3.0
+Usage:
+    main.py example
+    main.py (-h | --help)
+Options:
+    -h --help     Show this screen.
+"""
 
 
-def example():
-    card = ColorCard(img_format='png')
-    thread_pic = card.create_thread_pic('666')
-    lookup_file = 'lord_libidan_hexcodes.csv'
-    lookup = Reader.create_oklch_dict_from_hexcodes(lookup_file)
-    reference = lookup[thread_pic.thread_id]
-    csv_lines = [Writer.get_stats_csv_header()]
-    plotter = OklchPlotter(thread_pic, reference)
-    csv_lines.append(Writer.get_stats_csv_line(plotter))
-    Writer.save_stats_csv(csv_lines, 'stats_666.csv')
-    plotter.plot_combo_lch()
-    plotter.save('')
-    plotter.show()
+from docopt import docopt
+from Control import Control
 
 
 if __name__ == '__main__':
-    example()
+    args = docopt(__doc__)
+    Control(args)
 
 
