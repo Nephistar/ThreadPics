d73aad2bbfb61bdeab064d1451c033cc963060ec
Nephistar <51949062+Nephistar@users.noreply.github.com>
2026-08-07 20:56:41 +0200

    remove redundant code

--- ColorCard.py
+++ ColorCard.py
@@ -12,18 +12,15 @@ class ColorCard:
 
     def _create_thread_pic(self, thread_id: str, img_path: str) -> ThreadPic:
         thread_pic = ThreadPic(thread_id, img_path)
-        self.thread_pics.append(thread_pic)
         return thread_pic
 
     def _create_all_thread_pics(self):
-        thread_pics = []
         for file in os.listdir(self.dir_path):
             path = os.path.join(self.dir_path, file)
             if os.path.isfile(path):
                 thread_id = file[:file.rfind('.')]
                 if os.path.getsize(path) <= self.size_limit:
                     thread_pic = self._create_thread_pic(thread_id, path)
-                    thread_pics.append(thread_pic)
-        self.thread_pics = thread_pics
-        return thread_pics
+                    self.thread_pics.append(thread_pic)
+        return self.thread_pics
 
